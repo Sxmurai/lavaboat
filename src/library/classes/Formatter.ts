523b72f7e63bef80d5adc091b4a320eff03d8774
Samuraiii <57580886+Sxmurai@users.noreply.github.com>
2020-05-07 14:39:15 -0500

    migrate to @kyflx-dev/logger

--- src/library/classes/Formatter.ts
+++ src/library/classes/Formatter.ts
@@ -1,22 +1,14 @@
-import Logger, {
+import {
   Color,
-  ColorUtil,
   DefaultFormatter,
   DefaultFormatterColor,
-  LogLevel,
-} from "@ayanaware/logger";
-import fecha from "fecha";
+  Logger,
+} from "@kyflx-dev/logger";
 
-Logger.getDefaultTransport().setLevel(LogLevel.TRACE);
 Logger.setFormatter(
-  new (class BotLogger extends DefaultFormatter {
-    public formatTimestamp() {
-      return ColorUtil.getFormatter(Color.GRAY)(
-        fecha.format(Date.now() as any, "hh:mm MM/DD/YYYY")
-      );
-    }
-  })({
+  new DefaultFormatter({
     colorMap: new Map([
+      [DefaultFormatterColor.LOG_TIMESTAMP, Color.GRAY],
       [DefaultFormatterColor.LOG_PACKAGE_PATH, Color.BLUE],
       [DefaultFormatterColor.LOG_PACKAGE_NAME, Color.MAGENTA],
       [DefaultFormatterColor.LOG_UNIQUE_MARKER, Color.GRAY],
