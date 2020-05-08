ac1f9d06b88a5370bab817b77cb2ea1deab323ea
Samuraiii <57580886+Sxmurai@users.noreply.github.com>
2020-05-08 00:57:54 -0500

    update rest manager so playlist loading is a thing

--- src/library/classes/RestManager.ts
+++ src/library/classes/RestManager.ts
@@ -1,9 +1,5 @@
-import { AkairoClient } from "discord-akairo";
 import { TrackInfo, LoadTrackResponse } from "@kyflx-dev/lavalink-types";
 import fetch from "node-fetch";
-import { URLSearchParams } from "url";
-
-type identifiers = "ytsearch" | "scsearch";
 
 export class RestManager {
   public static async decodeTrack(track: string): Promise<TrackInfo> {
@@ -21,20 +17,17 @@ export class RestManager {
     return (await res.json()) || {};
   }
 
-  public static async loadTracks(
-    track: string,
-    identifier: identifiers
-  ): Promise<LoadTrackResponse> {
+  public static async loadTracks(track: string): Promise<LoadTrackResponse> {
     const { address, port, password } = config.get("nodes")[0];
 
-    const params = new URLSearchParams();
-    params.append("identifier", `${identifier}:${track}`);
-
-    const res = await fetch(`http://${address}:${port}/loadtracks?${params}`, {
-      headers: {
-        Authorization: password,
-      },
-    });
+    const res = await fetch(
+      `http://${address}:${port}/loadtracks?identifier=${track}`,
+      {
+        headers: {
+          Authorization: password,
+        },
+      }
+    );
 
     return (await res.json()) || ({} as LoadTrackResponse);
   }
