f4fbb4052c17f8047bca21674cb98fa164b15a81
Samuraiii <57580886+Sxmurai@users.noreply.github.com>
2020-05-09 19:51:39 -0500

    add song selection/better loadtype support

--- src/core/commands/music/Play.ts
+++ src/core/commands/music/Play.ts
@@ -77,37 +77,115 @@ export default class PlayCommand extends Command {
 
     if (!player.queue) player.queue = new LavaboatQueue(player);
 
-    let loadedTrack = await RestManager.loadTracks(track);
-    if (!["http:", "https:"].includes(track))
-      loadedTrack = await RestManager.loadTracks(
-        `${type === "youtube" ? "ytsearch:" : "scsearch"}:${track}`
-      );
-    if (!loadedTrack.tracks.length)
-      return message.util.send(
-        new LavaboatEmbed(message)
-          .setColor("#db3b3b")
-          .setDescription(`Nothing was found for your search query on ${type}.`)
-      );
-
-    if (loadedTrack.loadType === "PLAYLIST_LOADED")
-      loadedTrack.tracks.map(({ track }) =>
-        player.queue.add(track, message.author)
-      );
-    else player.queue.add(loadedTrack.tracks[0].track, message.author);
-
-    message.util.send(
-      new LavaboatEmbed(message)
-        .setThumbnail(
-          `https://i.ytimg.com/vi/${loadedTrack.tracks[0].info.identifier}/hqdefault.jpg`
-        )
-        .setDescription(
-          loadedTrack.loadType === "PLAYLIST_LOADED"
-            ? `Enqueued Playlist (\`${loadedTrack.tracks.length} Tracks\`):\n\n ${loadedTrack.playlistInfo.name}`
-            : `Enqueued Track:\n\n [${loadedTrack.tracks[0].info.title}](${loadedTrack.tracks[0].info.uri})`
-        )
+    const loadedTrack = await RestManager.loadTracks(
+      track.includes("https://")
+        ? track
+        : `${type === "youtube" ? "ytsearch" : "scsearch"}:${track}`
     );
 
-    if (!player.playing) await player.queue.start(message);
+    switch (loadedTrack.loadType) {
+      case "NO_MATCHES":
+      case "LOAD_FAILED":
+        return message.util.send(
+          new LavaboatEmbed(message)
+            .setColor("#db3b3b")
+            .setDescription(
+              `Nothing was found for your search query on ${type}.`
+            )
+        );
+        break;
+
+      case "PLAYLIST_LOADED":
+        loadedTrack.tracks.map(({ track }) =>
+          player.queue.add(track, message.author)
+        );
+
+        message.util.send(
+          new LavaboatEmbed(message)
+            .setThumbnail(
+              `https://i.ytimg.com/vi/${loadedTrack.tracks[0].info.identifier}/hqdefault.jpg`
+            )
+            .setDescription(
+              `Enqueued Playlist (\`${loadedTrack.tracks.length} Tracks\`):\n\n ${loadedTrack.playlistInfo.name}`
+            )
+        );
+
+        if (!player.playing) await player.queue.start(message);
+
+        break;
+
+      case "TRACK_LOADED":
+        player.queue.add(loadedTrack.tracks[0].track, message.author);
+
+        message.util.send(
+          new LavaboatEmbed(message)
+            .setThumbnail(
+              `https://i.ytimg.com/vi/${loadedTrack.tracks[0].info.identifier}/hqdefault.jpg`
+            )
+            .setDescription(
+              `Enqueued Track:\n\n [${loadedTrack.tracks[0].info.title}](${loadedTrack.tracks[0].info.uri})`
+            )
+        );
+
+        if (!player.playing) await player.queue.start(message);
+
+        break;
+
+      case "SEARCH_RESULT":
+        const tracks = loadedTrack.tracks.slice(0, 5);
+
+        const msg = await message.channel.send(
+          new LavaboatEmbed(message)
+            .setDescription(
+              tracks.map(
+                ({ info: { title, uri } }, index) =>
+                  `\`#${index + 1}\` [\`${title}\`](${uri})`
+              )
+            )
+            .setFooter(`Pick a track between 1-${tracks.length}`)
+        );
+
+        const filter = (msg: Message) => msg.author.id === message.author.id;
+
+        msg.channel
+          .awaitMessages(filter, { time: 3e4, errors: ["time"], max: 1 })
+          .then(async (collected) => {
+            const first = collected.first();
+
+            if (first.content.toLowerCase() === "cancel")
+              return message.util.send(`Cancelled the selection..`);
+
+            if (!this.handler.resolver.type("number")(msg, first.content))
+              return message.util.send(
+                `Yeah uh, you need to pick a valid number next time..`
+              );
+
+            player.queue.add(
+              tracks[Number(first.content) - 1].track,
+              first.author
+            );
+
+            message.util.send(
+              new LavaboatEmbed(message)
+                .setThumbnail(
+                  `https://i.ytimg.com/vi/${
+                    loadedTrack.tracks[Number(first.content) - 1].info
+                      .identifier
+                  }/hqdefault.jpg`
+                )
+                .setDescription(
+                  `Enqueued Track:\n\n [${
+                    loadedTrack.tracks[Number(first.content) - 1].info.title
+                  }](${loadedTrack.tracks[Number(first.content) - 1].info.uri})`
+                )
+            );
+
+            if (!player.playing) await player.queue.start(message);
+          })
+          .catch(() => msg.edit(`Well, cancelled the selection.`));
+
+        break;
+    }
   }
 
   public missingPermissions(user: any, permissions: string[]) {
