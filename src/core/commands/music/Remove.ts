2a66fa56daba81a206cf5baa3a514361f18fac2d
Samuraiii <57580886+Sxmurai@users.noreply.github.com>
2020-05-10 22:46:13 -0500

    djrole support

--- src/core/commands/music/Remove.ts
+++ src/core/commands/music/Remove.ts
@@ -21,6 +21,12 @@ export default class RemoveCommand extends Command {
         examples: ["remove 1", "remove 3"],
       },
       channel: "guild",
+      userPermissions: (msg: Message) => {
+        if (msg.member.hasPermission("ADMINISTRATOR")) return null;
+        const djRole = this.client.db.get(msg.guild.id, "config.djRole", null);
+
+        if (djRole && !msg.member.roles.cache.has(djRole)) return "DJ";
+      },
     });
   }
 
